81e684988e0d8e655abe4353b93c4fb0ca8bf8df
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-06-09 15:18:26 -0400

    Added more stuff

--- MicrosCashDrawer/MicrosCashDrawer.cs
+++ MicrosCashDrawer/MicrosCashDrawer.cs
@@ -3,36 +3,42 @@ using MicrosLibrary;
 
 namespace MicrosCashDrawer
 {
-    class MicrosCashDrawer
+    public class CashDrawer
     {
         private MicrosLibraryWrapper Micros = new MicrosLibraryWrapper();
         private Timer DrawerCheckTimer = new Timer();
-        private int PreviousDrawerStatus = -1;
+        private CashDrawerStatus PreviousDrawerStatus = CashDrawerStatus.UNKNOWN;
+        private uint CDNumber = 1;
         public event CashDrawerStatusChangedDelegate CashDrawerStatusChanged;
         public delegate void CashDrawerStatusChangedDelegate(CashDrawerStatus cashDrawerStatus);
-        public MicrosCashDrawer()
+        public CashDrawerStatus CashDrawerStatus { get; private set; } = CashDrawerStatus.UNKNOWN;
+        public CashDrawer(uint CashDrawerNumber = 1)
         {
+            CDNumber = CashDrawerNumber;
             Micros.CashDrawerVerifyPassword("000000", "0");
+            CashDrawerStatus = (CashDrawerStatus)Micros.CashDrawerStatus(CDNumber);
             DrawerCheckTimer.Interval = 10;
             DrawerCheckTimer.Elapsed += DrawerCheckTimer_Elapsed;
             DrawerCheckTimer.Start();
         }
         public int OpenCashDrawer()
         {
-            return Micros.OpenCashDrawer(1);
+            return Micros.OpenCashDrawer(CDNumber);
         }
+        
         private void DrawerCheckTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            int CurrentDrawerStatus = Micros.CashDrawerStatus(1);
-            if (PreviousDrawerStatus != CurrentDrawerStatus && CashDrawerStatusChanged != null)
+            CashDrawerStatus = (CashDrawerStatus)Micros.CashDrawerStatus(CDNumber);
+            if (PreviousDrawerStatus != CashDrawerStatus && CashDrawerStatusChanged != null)
             {
-                CashDrawerStatusChanged.Invoke((CashDrawerStatus)CurrentDrawerStatus);
+                CashDrawerStatusChanged.Invoke(CashDrawerStatus);
             }
-            PreviousDrawerStatus = CurrentDrawerStatus;
+            PreviousDrawerStatus = CashDrawerStatus;
         }
     }
-    enum CashDrawerStatus
+    public enum CashDrawerStatus
     {
+        UNKNOWN = -1,
         OPEN = 1,
         CLOSED = 0
     }
